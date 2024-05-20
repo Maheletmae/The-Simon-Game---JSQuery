d513f7605cc5f4da99b37e6e99c930cbc8368f8b
Maheletmae <88266126+Maheletmae@users.noreply.github.com>
2024-05-20 11:39:17 +0200

    Update game.js
    Adding the code for the text update and the interaction code with the color button (sound, color, fading)

--- game.js
+++ game.js
@@ -2,21 +2,56 @@ var colors = ["red", "blue", "green", "yellow"];
 var colorPattern = [];
 var userPattern = [];
 
+// Game Sequence //
 function randomColorSequence() {
   var rn = Math.floor(Math.random() * 3) + 1; // rn = random number between 0 to 3
-  var newColorPattern = colors[rn]; // random color registered
+  var newColorPattern = colors[rn]; // register random color in arrow
   colorPattern.push(newColorPattern);
 
+  // action for this color/button
   $("#" + newColorPattern)
     .fadeOut(100)
-    .fadeIn(100); // action for this color/button
+    .fadeIn(100);
 
-  var colorSound = new Audio("./sounds/" + newColorPattern + ".mp3");
+  // play a sound related to random color
+  playSound(newColorPattern);
+}
+
+// When click a color button //
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userPattern.push(userChosenColour); // register it in user color arrow
+  $("#" + userChosenColour)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(userChosenColour);
+  greyButton(userChosenColour);
+});
+
+function playSound(name) {
+  var colorSound = new Audio("./sounds/" + name + ".mp3");
   colorSound.play();
 }
 
-$(".btn").click(function () {
-  var userChosenColour = $(this).attr("id");
-  userPattern.push(userChosenColour);
-  console.log(userPattern);
+function greyButton(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+// Text update //
+var started = false;
+var level = 0;
+
+$(document).keypress(function () {
+  if (started == false) {
+    started = true;
+    $("h1").text("Level " + level);
+  } else {
+    level = level + 1;
+    $("h1").text("Level " + level);
+  }
+
+  randomColorSequence();
 });
